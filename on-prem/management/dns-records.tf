eeac8e11aa7785a88887f573ce435169936f50a6
th3-s7r4ng3r <gvinura@gmail.com>
2025-11-18 06:59:47 +0530

    Ref: update harbor service name

--- on-prem/management/dns-records.tf
+++ on-prem/management/dns-records.tf
@@ -80,9 +80,9 @@ module "harbor_dns" {
   cf_dns_record_value = var.cf_default_record_value
 
   # NGINX Proxy Manager variables
-  nginx_proxy_manager_forward_protocol = "https"
-  nginx_proxy_manager_forward_service  = "harbor.${var.namespace}.${var.cluster_service_domain}"
-  nginx_proxy_manager_forward_port     = 443
+  nginx_proxy_manager_forward_protocol = "http"
+  nginx_proxy_manager_forward_service  = "harbor-portal.${var.namespace}.${var.cluster_service_domain}"
+  nginx_proxy_manager_forward_port     = 80
 
   depends_on_resource = [module.jenkins_dns] # To prevent 500 error when letsencrypt tries to create mutiple certificates
 }
