507bf8c348776337a507965ffa3c194392017904
th3-s7r4ng3r <gvinura@gmail.com>
2025-11-28 01:24:47 +0530

    Ref: Updated argocd variable

--- on-prem/env-dev/applications.tf
+++ on-prem/env-dev/applications.tf
@@ -6,7 +6,7 @@ module "argocd_helabooking_dev_app" {
   source = "../cluster-templates/application"
 
   argocd_application_name      = "helabooking-dev"
-  gitops_branch                = "dev"
+  argocd_repo_branch           = "dev"
   argocd_application_path      = "overlays/env-dev"
   argocd_application_namespace = "env-dev"
 }
