f57d31cd9d7a342fd7fba28d4468cfd5b27cbc53
th3-s7r4ng3r <gvinura@gmail.com>
2025-12-03 23:54:26 +0530

    Ref: updated ingress dns

--- on-prem/env-dev/dns-records.tf
+++ on-prem/env-dev/dns-records.tf
@@ -15,6 +15,8 @@ module "helabooking_ingress_dns" {
   nginx_proxy_manager_forward_protocol = "https"
   nginx_proxy_manager_forward_service  = "traefik.${var.namespace}.${var.cluster_service_domain}"
   nginx_proxy_manager_forward_port     = 443
+
+  depends_on_resource = [module.rabbitmq_dns] # To prevent 500 error when letsencrypt tries to create mutiple certificates
 }
 
 
@@ -34,8 +36,6 @@ module "rabbitmq_dns" {
   nginx_proxy_manager_forward_protocol = "http"
   nginx_proxy_manager_forward_service  = "rabbitmq.${var.namespace}.${var.cluster_service_domain}"
   nginx_proxy_manager_forward_port     = 15672
-
-  depends_on_resource = [module.helabooking_ingress_dns] # To prevent 500 error when letsencrypt tries to create mutiple certificates
 }
 
 ################################ Supporting Service Related DNS Records ################################
