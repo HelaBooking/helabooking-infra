57c3179302bbb9f1a32f4b52598f193c67915617
th3-s7r4ng3r <gvinura@gmail.com>
2025-12-04 00:06:44 +0530

    Ref: update ingress dns target

--- on-prem/env-dev/dns-records.tf
+++ on-prem/env-dev/dns-records.tf
@@ -12,9 +12,9 @@ module "helabooking_ingress_dns" {
   cf_dns_record_value = var.cf_default_record_value
 
   # NGINX Proxy Manager variables
-  nginx_proxy_manager_forward_protocol = "https"
+  nginx_proxy_manager_forward_protocol = "http"
   nginx_proxy_manager_forward_service  = "traefik.${var.namespace}.${var.cluster_service_domain}"
-  nginx_proxy_manager_forward_port     = 443
+  nginx_proxy_manager_forward_port     = 80
 
   depends_on_resource = [module.rabbitmq_dns] # To prevent 500 error when letsencrypt tries to create mutiple certificates
 }
