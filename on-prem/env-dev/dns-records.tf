c3fb626650427ced764b01ca5d180763c4720221
th3-s7r4ng3r <gvinura@gmail.com>
2025-12-03 16:09:26 +0530

    Ref: Temporary exposing user api

--- on-prem/env-dev/dns-records.tf
+++ on-prem/env-dev/dns-records.tf
@@ -17,6 +17,20 @@ module "helabooking_frontend_dns" {
   nginx_proxy_manager_forward_port     = 80
 }
 
+# Templ Deploying User API
+module "helabooking_user_api_dns" {
+  source = "../cluster-templates/dns-record"
+
+  # Cloudflare variables
+  cf_dns_record_name  = "user.api.${var.cf_default_root_domain}"
+  cf_dns_record_value = var.cf_default_record_value
+
+  # NGINX Proxy Manager variables
+  nginx_proxy_manager_forward_protocol = "http"
+  nginx_proxy_manager_forward_service  = "user-service-svc.${var.namespace}.${var.cluster_service_domain}"
+  nginx_proxy_manager_forward_port     = 8081
+}
+
 
 ################################ App Service Related DNS Records ################################
 # Deploying Records - Cloudflare & NGINX Proxy Manager:
