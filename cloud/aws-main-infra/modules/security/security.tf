891de02b51ce00ae734c2051e55300700735e5be
th3-s7r4ng3r <gvinura@gmail.com>
2025-12-19 03:34:01 +0530

    Ref: udpate sg rule

--- cloud/aws-main-infra/modules/security/security.tf
+++ cloud/aws-main-infra/modules/security/security.tf
@@ -53,13 +53,6 @@ resource "aws_security_group" "vpn" {
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion.id]
   }
-  ingress {
-    description     = "SSH from VPN Node"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.vpn.id]
-  }
 
   egress {
     from_port   = 0
